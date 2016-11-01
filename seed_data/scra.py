0b5c9cb1a310dc83eb4964b349273a22bf7eb807
Chung Nguyen <chung.nguyen@gmail.com>
2016-11-01 00:53:03 +0000

    Results from Scrapy file are clean; ready to scrape website!

--- seed_data/scra.py
+++ seed_data/scra.py
@@ -16,25 +16,31 @@ class FareSpider(scrapy.Spider):
         header = response.css('h1.ttl').extract_first()
         lst = header.split()
 
-        def extract(string, start='(', stop=')'):
+        def extract_parentheses(string, start='(', stop=')'):
             """Got the function from here: http://bit.ly/2e6ZqM2"""
             return string[string.index(start)+1:string.index(stop)]
 
+        lst_codes = []
+
         for item in lst:
             if "(" in item:
-                extract(item)
+                lst_codes.append(extract_parentheses(item))
         
-        # Fare area with price and cheapest month info. Sample data: u'<div 
-        # class="div7">From:Zurich<br>To:Toronto<br>Lowest price found:771.5
-        # <br>Average price: 1063<br>Cheapest months to travel: November<br>
-        # </div>'
+        """Fare area with price and cheapest month info. Sample data: 
+            u'<div class="div7">From:Zurich<br>To:Toronto<br>Lowest price 
+            found:771.5 <br>Average price: 1063<br>Cheapest months to travel: 
+            November<br></div>
+        '"""
+
+        def extract_colon(string, start=':'):
+            return string[string.index(start)+1:].strip()
 
         fare = response.css('div.div7').extract_first()
         fare = fare.encode('ascii', 'ignore').split("<br>")
         yield {
-            'from': fare[0],
-            'to': fare[1],
-            'lowest_price': fare[2],
-            'average_price': fare[3],
-            'cheapest_month': fare[4],
+            'depart': lst_codes[0],
+            'arrive': lst_codes[1],
+            'lowest_price': extract_colon(fare[2]),
+            'average_price': extract_colon(fare[3]),
+            'cheapest_month': extract_colon(fare[4]),
             }
