62f30636ef9640e41ddf5381c414400a21056390
Chung Nguyen <chung.nguyen@gmail.com>
2016-11-03 22:16:32 +0000

    Updated '/' route to pass over a dynamic month-year calendar to the 'when' search box in the homepage.

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 """Airfare search: https://github.com/wordswithchung/hackbright-project"""
 
-from datetime import datetime
+import calendar
+from datetime import date, datetime, timedelta
 from flask import (Flask, jsonify, render_template, redirect, 
                    request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
@@ -19,36 +20,43 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
+
+    today = datetime.today()
+    months = [today]
+    # http://stackoverflow.com/a/12736311
     
-    return render_template("homepage.html")
+    for i in range(11):
+        days_in_month = calendar.monthrange(today.year, today.month)[1]
+        month = today + timedelta(days=days_in_month)
+        months.append(month)
+        today = month
+    
+    return render_template("homepage.html", months=months)
 
 @app.route('/search', methods=['POST'])
 def search():
     """User searches for flight."""
 
-    # depart = request.form.get("depart")
-    # month = request.form.get("month")
-    # duration = request.form.get("duration")
+    depart = request.form.get("depart")
+    month = request.form.get("month")
+    duration = request.form.get("duration")
 
-    # things to implement: depart should pull from airports database
-    # for autocomplete
-
-    """LOGIC V2
-
-    When we get user's depart and month:
-    - if depart exists in airfares table (Airfare), find the best $/mile
-    and ping the API for those arrival airport codes
-    - in addition, find the arrival airport codes which has the cheapest_
-    month equal to user's month. Diversify the distance from user's depart
-    and ping the API for those arrival airport codes
-    """
-
-    # SECTION ONE ######
     user_port = Port.query.filter_by(code=depart).first()
+
+
+
+    print depart
+    print month
+    print duration
+
+
 
     ports_to_search = set()
     
-
+@app.route('/autocomplete_port')
+def autocomplete_airport_search():
+    #https://designshack.net/articles/javascript/create-a-simple-autocomplete-with-html5-jquery/
+    pass
 
 
 if __name__ == "__main__":
@@ -60,4 +68,4 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
---
+++ static/main.js
@@ -0,0 +1,11 @@
+//search port
+
+  // $('#autocomplete_port').autocomplete({
+  //   lookup: currencies,
+
+    
+  //   onSelect: function (suggestion) {
+  //     var thehtml = '<strong>Currency Name:</strong> ' + suggestion.value + ' <br> <strong>Symbol:</strong> ' + suggestion.data;
+  //     $('#outputcontent').html(thehtml);
+  //   }
+  // });
